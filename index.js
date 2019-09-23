0b9bc6d1b7b1adf366cdac1739c86345a0e8d7e9
JoaquinGomezCabido <joaquingomezcabido@gmail.com>
2019-09-23 08:54:37 +0100

    Done.

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
-const testVar = {}
+// const testVar = {};
 
-function testFunc() {
-  return "hi"
+// function testFunc() {
+// 	return "hi";
+// }
+
+function superbowlWin(history) {
+	let solution = history.find(function(event) {
+		return event.result === "W";
+	});
+	return !!solution ? solution.year : undefined;
 }
